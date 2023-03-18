e3c5579b05639c7bf956a11b9a61ea2a2d80ee94
joseLuís <joseluis@andamira.net>
2023-03-18 10:41:25 +0100

    misc. project updates
    - update cargo manifest
      - add `nightly` feature.
      - update dependencies.
      - refactor.
    - simplify lint allowances.
    - update CIs.

--- src/lib.rs
+++ src/lib.rs
@@ -43,13 +43,6 @@
 //
 
 #![warn(clippy::all)]
-#![allow(
-    clippy::float_arithmetic,
-    clippy::implicit_return,
-    clippy::needless_return,
-    clippy::blanket_clippy_restriction_lints,
-    clippy::pattern_type_mismatch
-)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![forbid(unsafe_code)]
 
